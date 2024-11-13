7d4c3efff84040a5a774a5c43b35bf56d1b773d3
Nayara Correia <108832805+NahCorreia@users.noreply.github.com>
2024-11-13 20:54:24 -0300

    hjklç

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function showslides() {
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.opacity = 1;
     setTimeout (showslides, 3000);
-   
+}
 
 function irParaProdutos() {
     window.location.href = "produtos.html";
